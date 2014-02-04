49b60ca4ce2b50802a5d407bb2e929b5c5223540
Matt Newton <newton10471@gmail.com>
2014-02-04 06:17:17 -0500

    first passing test

--- xyz_service_spec.rb
+++ xyz_service_spec.rb
@@ -2,10 +2,23 @@ require_relative './xyz_service'
 
 describe XYZService do
 
-  subject { XYZService.xyz_filename(stub) }
+  let(:target) do 
+    messages = {
+      :publish_on => Date.new(2012, 3, 14),
+      :xyz_category_prefix => 'abc',
+      :kind => 'unicorn',
+      :personal? => false,
+      :age => 50,
+      :id => 1337,
+      :title => 'magic & superglue'
+    }
+    stub(:target, messages)
+  end
+
+  subject { XYZService.xyz_filename(target) }
 
   it 'works' do
-    subject.should eq('something')
+    subject.should match(/14abcunicorn_1337_[0-9a-f]{8}_magicsuper\.jpg/)
   end
 
 end
